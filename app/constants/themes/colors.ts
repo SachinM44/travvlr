e285877a7f525085b07141726a81808af21ed159
SachinM44 <sachin69778@gmail.com>
2026-02-03 16:34:15 +0530

    feat/theme : addeding thems with zustand packege

---
+++ app/constants/themes/colors.ts
@@ -0,0 +1,129 @@
+export const themes = {
+  "default": {
+    "primary": {
+      "darker": "#73024A",
+      "dark": "#8C0855",
+      "default": "#AB3874",
+      "light": "#E26AA84D",
+      "lighter": "#F9BEDD33"
+    },
+
+    "secondary": {
+      "darker": "#BA1748",
+      "dark": "#BA1748",
+      "default": "#D54A7D",
+      "light": "#FBBCD34D",
+      "lighter": "#FEC7DC33"
+    },
+
+    "neutral": {
+      "heading": "#3C021E",
+      "body": "#37011ECC",
+      "labels": "#37011EB2",
+      "alternative": "#37011E99",
+      "placeholder": "#37011E66",
+      "default": "#4B022533",
+      "border": "#55022A1A",
+      "dark": "#5F012F0A",
+      "darker": "#5F012F12",
+      "white": "#FFFFFF"
+    },
+
+    "red": {
+      "darker": "#880214",
+      "dark": "#AB0E22",
+      "default": "#DD4C56",
+      "light": "#FDA0AC4D",
+      "lighter": "#FFD1D74D"
+    },
+
+    "yellow": {
+      "darker": "#AE7700",
+      "dark": "#D6A800",
+      "default": "#F6D604",
+      "light": "#F5ED004D",
+      "lighter": "#FFF68733"
+    },
+
+    "green": {
+      "darker": "#017427",
+      "dark": "#1B9343",
+      "default": "#2CB158",
+      "light": "#87E8A84D",
+      "lighter": "#ABF5C433"
+    },
+
+    "orange": {
+      "darker": "#B11506",
+      "dark": "#DD3627",
+      "default": "#F85C4D",
+      "light": "#FEA29A4D",
+      "lighter": "#FFD0CC4D"
+    },
+
+    "teal": {
+      "darker": "#006654",
+      "dark": "#09957C",
+      "default": "#2FB69E",
+      "light": "#82E8D64D",
+      "lighter": "#B7FAEE4D"
+    },
+
+    "purple": {
+      "darker": "#1B137C",
+      "dark": "#3C349D",
+      "default": "#645DBE",
+      "light": "#C1BDF24D",
+      "lighter": "#E3E0FF4D"
+    },
+
+    "pink": {
+      "darker": "#650F66",
+      "dark": "#650F66",
+      "default": "#CB47CD",
+      "light": "#F6BBF74D",
+      "lighter": "#FEE1FF4D"
+    },
+
+    "blue": {
+      "darker": "#002F70",
+      "dark": "#064DB2",
+      "default": "#2B7FF3",
+      "light": "#A8CCFF4D",
+      "lighter": "#D6E7FF4D",
+      "tooltip": "#061A4E"
+    },
+
+    "cyan": {
+      "darker": "#006680",
+      "dark": "#0D87A5",
+      "default": "#52BFDA",
+      "light": "#89E4FA4D",
+      "lighter": "#B8F1FF4D"
+    },
+
+    "lime": {
+      "darker": "#3D630D",
+      "dark": "#568816",
+      "default": "#84C135",
+      "light": "#B5EC6F4D",
+      "lighter": "#D4FAA34D"
+    },
+
+    "violet": {
+      "darker": "#4C178C",
+      "dark": "#5C26A0",
+      "default": "#8B4DD6",
+      "light": "#CAAAF24D",
+      "lighter": "#E1CAFF33"
+    },
+
+    "maroon": {
+      "darker": "#A72E2F",
+      "dark": "#972022",
+      "default": "#BF5960",
+      "light": "#D86A6B4D",
+      "lighter": "#EFA5A633"
+    }
+  }
+}
