b722ffec196467c0897fbfe3bf39903b18c96b19
SachinM44 <sachin69778@gmail.com>
2026-02-18 14:50:46 +0530

    feat/flash: added flash screen to the app entry

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
 import { Button } from '@/components/Button';
+import { Input } from '@/components/Input';
 import React from 'react';
 import { View, Text, Pressable } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Login() {
   return (
-    <Pressable>
+   <SafeAreaView>
+     <Pressable>
       <View style=''>
         <Text> hell there </Text>
         <Button size='medium' buttonType='primary' >hello there dd</Button>
+        <Input state='error' placeholder='hello there '/>
       </View>
     </Pressable>
+   </SafeAreaView>
   );
 }
